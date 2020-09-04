4c30d307a66b1692e2d6a76b80ce30bb24c246d1
Enai63 <Toha76@yandex.ru>
2020-09-04 23:01:49 +0400

    new data and profileUser

--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -9,8 +9,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -36,11 +36,17 @@ public class UserService implements UserDetailsService {
             return false;
         }
 
-        user.setActive(true);                                        //TODO need false is reg no code
+        user.setActive(false);                         //true               //TODO need false is reg no code
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
 
+        sendMessage(user);
+
+        return true;
+    }
+
+    private void sendMessage(User user) {
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
                     "Hello, %s! \n" +
@@ -50,8 +56,6 @@ public class UserService implements UserDetailsService {
             );
             smptMailSender.send(user.getEmail(), "Activate code", message);
         }
-
-        return true;
     }
 
 
@@ -61,8 +65,49 @@ public class UserService implements UserDetailsService {
             return false;
         }
         user.setActivationCode(null);
+        user.setActive(true);
         userRepo.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+        if (isEmailChanged) {
+            user.setEmail(email);
+            if (!StringUtils.isEmpty(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+        userRepo.save(user);
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
     }
 }
--- src/main/resources/templates/parts/login.ftl
+++ src/main/resources/templates/parts/login.ftl
@@ -1,6 +1,5 @@
 <#macro login path isRegisterForm>
     <form action="${path}" method="post" xmlns="http://www.w3.org/1999/html">
-
         <div class="form-group row">
             <label class="col-sm-2 col-form-label">User Name :</label>
             <div class="col-sm-6">
@@ -33,7 +32,6 @@
         <input type="hidden" name="_csrf" value="${_csrf.token}"/>
            <#if !isRegisterForm><a href="/registration">Add new user</a></#if>
             <button class="btn btn-primary" type="submit"><#if isRegisterForm>Create<#else>Sign In</#if></button>
-
     </form>
 </#macro>
 <#macro logout>
